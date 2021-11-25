c55e290cee7288fdbf9746272bfeb38b97e8691a
Konstantin <flyinnet@yandex.ru>
2021-11-25 03:06:19 +0300

    test_web_server

--- webserver/http_server.py
+++ webserver/http_server.py
@@ -24,4 +24,4 @@ def data():
     return my.print_text(content['word_1'],content['word_2'])
 
 if __name__ == '__main__':
-    app.run(host='localhost', port='10000',debug=True)
+    app.run(host='127.0.0.1', port='10000')
